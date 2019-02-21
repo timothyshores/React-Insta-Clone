f36b01cb173852c161507331cd8328c938a31bec
Timothy Shores <timothymshores@gmail.com>
2019-02-21 16:27:49 -0600

    Refactored SearchBar component using styled components library

--- instagram/src/components/SearchBar/SearchBar.js
+++ instagram/src/components/SearchBar/SearchBar.js
@@ -1,29 +1,51 @@
 import React from 'react';
 import IGLogo from '../../assets/instagram-text.png';
-import './SearchBar.css';
+import styled from 'styled-components'
+
+const SearchBarWrapper = styled.div`
+  border-bottom: 1px solid #3333;
+  height: 80px;
+  line-height: 70px;
+  display: flex;
+  justify-content: space-between;
+  background: #ffffff;
+`;
+
+const ImageWrapper = styled.div`
+    height: 50px;
+    margin: 20px 10px;
+`;
+
+const SocialWrapper = styled.div`
+    display: flex;
+`;
+
+const Social = styled.div`
+    width: 50px;
+`;
 
 const SearchBar = props => {
     return (
-        <div className="search-bar-wrapper">
-            <div className="image-wrapper">
-                <img alt="instagram logo" src={IGLogo} className="logo-image" />
-            </div>
+        <SearchBarWrapper>
+            <ImageWrapper>
+                <img alt="instagram logo" src={IGLogo} style={{ height: "100%" }} />
+            </ImageWrapper>
             <div>
                 <input type="text" placeholder="Search" onKeyDown={props.searchPosts} />
             </div>
-            <div className="social-wrapper">
-                <div className="social">
+            <SocialWrapper>
+                <Social>
                     <i className="fa fa-compass" />
-                </div>
-                <div className="social">
+                </Social>
+                <Social>
                     <i className="fa fa-heart" />
-                </div>
-                <div className="social">
+                </Social>
+                <Social>
                     <i className="fa fa-user-circle" />
-                </div>
-            </div>
-        </div>
-    );
+                </Social>
+            </SocialWrapper>
+        </SearchBarWrapper>
+    )
 };
 
 export default SearchBar;
