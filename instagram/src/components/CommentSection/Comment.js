10309c4e37c2f64b60b2339a85e4524e802fab0f
Timothy Shores <timothymshores@gmail.com>
2019-02-21 16:35:56 -0600

    Refactored Comment component using styled components library

--- instagram/src/components/CommentSection/Comment.js
+++ instagram/src/components/CommentSection/Comment.js
@@ -1,13 +1,31 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import './Comment.css';
+import styled from 'styled-components'
+
+// import './Comment.css';
+
+const CommentText = styled.div`
+    text-align: left;
+    padding: 2px;
+    margin-left: 4px;
+`;
+
+const CommentSpan = styled.span`
+    font-weight: 300;
+    font-size: 14px;
+`;
+
+const user = styled.span`
+    font-weight: 500;
+    font-size: 12px;
+`;
 
 const Comment = props => {
     return (
-        <div className="comment-text">
-            <span className="comment">{props.comment.text}</span>{' '}
+        <CommentText>
+            <CommentSpan>{props.comment.text}</CommentSpan>{' '}
             <span className="user">-{props.comment.username}</span>
-        </div>
+        </CommentText>
     );
 };
 
