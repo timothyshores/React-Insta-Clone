d077597e9a1cc0cc572014861a7cd5724168beb2
Timothy Shores <timothymshores@gmail.com>
2019-03-25 17:20:23 -0500

    Style SearchBar component

--- instagram/src/components/SearchBar/SearchBar.js
+++ instagram/src/components/SearchBar/SearchBar.js
@@ -1,9 +1,26 @@
 import React from 'react';
+import instagramLogo from './instagram-logo.jpg';
+import discoverIcon from './discover-icon.png';
+import profileIcon from './profile-icon.png';
 import './SearchBar.css';
 
 const SearchBar = props => {
     return (
-        <h2>SearchBar Component</h2>
+        <div className="search-bar">
+            <div className="left-column">
+                <i className="instagram-icon" class="fab fa-instagram fa-2x"></i>
+                <span className="vertical-divider"> | </span>
+                <img className="instagram-logo" src={instagramLogo}></img>
+            </div>
+            <div className="middle-column">
+                <input type="text" className="search-bar" placeholder="🔍 Search"></input>
+            </div>
+            <div className="right-column">
+                <img className="discover-icon" src={discoverIcon}></img>
+                <p className="heart-icon">♡</p>
+                <img className="profile-icon" src={profileIcon}></img>
+            </div>
+        </div>
     );
 };
 
