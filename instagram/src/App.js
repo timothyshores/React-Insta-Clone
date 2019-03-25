a3ab3436b256c2cc164bc142b539e3c915baad32
Timothy Shores <timothymshores@gmail.com>
2019-03-25 12:29:58 -0500

    Import data into App.js

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -1,10 +1,22 @@
 import React, { Component } from 'react';
-import './App.css';
 import SearchBar from './components/SearchBar/SearchBar';
 import PostContainer from './components/PostContainer/PostContainer';
 import CommentSection from './components/CommentSection/CommentSection';
+import data from './dummy-data';
+import './App.css';
 
 class App extends Component {
+    constructor() {
+        super();
+        this.state = {
+            data: []
+        };
+    }
+
+    componentDidMount() {
+        this.setState({ data: data });
+    }
+
     render() {
         return (
             <div className="App">
