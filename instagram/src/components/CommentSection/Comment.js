b8a55088da6cfdbfd10ec60df98c11e9c7e50655
Timothy Shores <timothymshores@gmail.com>
2019-03-26 17:47:00 -0500

    Refactor CommentSection component to stateful class based component and add functionality to add new comment

--- instagram/src/components/CommentSection/Comment.js
+++ instagram/src/components/CommentSection/Comment.js
@@ -3,7 +3,6 @@ import PropTypes from 'prop-types';
 import './Comment.css';
 
 const Comment = props => {
-    console.log('Comment props', props.comment);
     const { username, text } = props.comment;
     return (
         <div className="comment">
--- instagram/src/components/CommentSection/CommentSection.js
+++ instagram/src/components/CommentSection/CommentSection.js
@@ -3,25 +3,55 @@ import PropTypes from 'prop-types';
 import Comment from './Comment'
 import './CommentSection.css';
 
-const CommentSection = props => {
-    const commentsArray = props.comments;
-    // console.log('CommentSection props', commentsArray);
-    return (
-        <div className="comment-section">
-            {console.log('commentsArray', commentsArray)}
-            {commentsArray.map((comment, index) => (
-                <Comment comment={comment} key={index} />
-            ))}
-            <div className="add-new comment">
-                <input type="text" className="add-comment" placeholder="Add a comment..."></input>
+class CommentSection extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            comments: [],
+            newComment: ''
+        };
+    }
+
+    componentDidMount() {
+        this.setState({ comments: this.props.comments });
+    }
+
+    handleChanges = event => {
+        this.setState({ newComment: event.target.value });
+    }
+
+    handleSubmit = event => {
+        this.setState({
+            comments: [...this.props.comments, {
+                username: 'timothyshores',
+                text: this.state.newComment
+            }]
+        });
+    }
+
+    render() {
+        return (
+            <div className="comment-section">
+                {this.state.comments.map((comment, index) => (
+                    <Comment comment={comment} key={index} />
+                ))}
+                <div className="add-new comment">
+                    <input
+                        type="text"
+                        className="add-comment"
+                        placeholder="Add a comment..."
+                        value={this.state.newComment}
+                        onChange={this.handleChanges}
+                    />
+                    <button onClick={this.handleSubmit}> Post </button>
+                </div>
             </div>
-        </div>
-    );
-};
+        );
+    }
+}
 
 CommentSection.propTypes = {
 
 };
-
 
 export default CommentSection;
