1c68c2cc2a3bb4f84f14c25352d24e972ca6c823
Timothy Shores <timothymshores@gmail.com>
2019-03-28 12:21:18 -0500

    Refactor PostContainer using styled components

--- instagram/src/components/PostContainer/PostContainer.js
+++ instagram/src/components/PostContainer/PostContainer.js
@@ -1,9 +1,49 @@
 import React from 'react';
-import './PostContainer.css';
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
 import CommentSection from '../CommentSection/CommentSection';
 import comment from './comment.png';
 
+const PostContainerWrapper = styled.div`
+    max-width: 50vw;
+    margin: 15px auto;
+`;
+
+const UsernameAvatarWrapper = styled.div`
+    display: flex;
+    align-items: center;
+`;
+
+const Avatar = styled.img`
+    height: 20px;
+`;
+
+const Username = styled.p`
+    margin-left: 15px;
+    font-weight: 600;
+`;
+
+const LikeCommentIcons = styled.div`
+    display: flex;
+    align-items: baseline;
+`;
+
+const HeartIcon = styled.p`
+    font-size: 2rem;
+    font-weight: 100;
+    margin: 0;
+`;
+
+const CommentIcon = styled.img`
+    height: 25px;
+    margin-left: 10px;
+`;
+
+const Likes = styled.div`
+    text-align: left;
+    font-weight: 600;
+    margin: 10px 0;
+`;
 
 class PostContainer extends React.Component {
     constructor(props) {
@@ -28,23 +68,29 @@ class PostContainer extends React.Component {
     render() {
         const { thumbnailUrl, username, imageUrl } = this.state.posts;
         return (
-            <div className="post-container">
-                <div className="username-avatar">
-                    <img className="avatar" src={thumbnailUrl} alt="user avatar" />
-                    <p className="username">{username}</p>
-                </div>
+            <PostContainerWrapper>
+                <UsernameAvatarWrapper>
+                    <Avatar
+                        src={thumbnailUrl}
+                        alt="user avatar"
+                    />
+                    <Username>
+                        {username}
+                    </Username>
+                </UsernameAvatarWrapper>
                 <div className="image">
                     <img src={imageUrl} alt="users post" />
                 </div>
-                <div className="like-comment-buttons">
-                    <p onClick={this.likePost} className="heart-icon">♡</p>
-                    <img className="comment-icon" src={comment} alt="comment icon" />
-                </div>
-                <div className="likes">
-                    <p>{this.state.likes} likes</p>
-                </div>
+                <LikeCommentIcons>
+                    <HeartIcon onClick={this.likePost}>♡</HeartIcon>
+                    <CommentIcon
+                        src={comment}
+                        alt="comment icon"
+                    />
+                </LikeCommentIcons>
+                <Likes>{this.state.likes} likes </Likes>
                 <CommentSection comments={this.state.posts.comments} />
-            </div>
+            </PostContainerWrapper>
         );
     }
 }
